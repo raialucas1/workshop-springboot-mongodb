b0222d71b89c2aeadfb28496dd9d838e36a2d24a
Raiã Lucas <raialucas@hotmail.com>
2024-04-17 09:12:42 -0500

    Adicionando Comentários aos posts

--- src/main/java/com/raialucas/workshopmongo/config/Instantiation.java
+++ src/main/java/com/raialucas/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.raialucas.workshopmongo.domain.Post;
 import com.raialucas.workshopmongo.domain.User;
 import com.raialucas.workshopmongo.dto.AuthorDTO;
+import com.raialucas.workshopmongo.dto.CommentDTO;
 import com.raialucas.workshopmongo.repositories.PostRepository;
 import com.raialucas.workshopmongo.repositories.UserRepository;
 
@@ -35,8 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		Post p1= new Post(null, sdf.parse("12/09/2005"), "Partiu viagem!", "Vou viajar para França. Abraços!",new AuthorDTO(u1));
 		Post p2= new Post(null,sdf.parse("28/04/2019"),"Dia Horrível!","Apostei minha casa em um jogo de futebol e perdi. Lamentável",new AuthorDTO(u1));
 		
+		CommentDTO c1= new CommentDTO("Boa viagem mano!",sdf.parse("12/09/2033"),new AuthorDTO(u1));
+		CommentDTO c2= new CommentDTO("Aproveite",sdf.parse("12/03/2002"),new AuthorDTO(u1));
+		CommentDTO c3= new CommentDTO("KKKKKKKKKKKK",sdf.parse("23/02/2312"),new AuthorDTO(u3));
 		
-		
+		p1.getComments().addAll(Arrays.asList(c1,c2));
+		p2.getComments().addAll(Arrays.asList(c3));
 		postRepository.saveAll(Arrays.asList(p1,p2));
 		
 		u1.getPosts().addAll(Arrays.asList(p1,p2));
--- src/main/java/com/raialucas/workshopmongo/domain/Post.java
+++ src/main/java/com/raialucas/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.raialucas.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.raialucas.workshopmongo.dto.AuthorDTO;
+import com.raialucas.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments=new ArrayList<>();
 
 	public Post() {
 
@@ -87,6 +92,10 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
 	}
 
 }
