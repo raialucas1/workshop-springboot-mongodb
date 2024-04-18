9f63dfefebadb1fc7c8fc26fade8fe9f7cf10600
Raiã Lucas <raialucas@hotmail.com>
2024-04-18 13:10:22 -0500

    Consulta com vários critérios

--- src/main/java/com/raialucas/workshopmongo/services/PostService.java
+++ src/main/java/com/raialucas/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.raialucas.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,20 +14,24 @@ import com.raialucas.workshopmongo.services.exceptions.ObjectNotFoundException;
 @Service
 public class PostService {
 
-	
 	@Autowired
 	private PostRepository repository;
-	
+
 	public Post findById(String id) {
-		Optional<Post> post=repository.findById(id);
-		if(post.isEmpty()) {
+		Optional<Post> post = repository.findById(id);
+		if (post.isEmpty()) {
 			throw new ObjectNotFoundException("Post não encontrado");
 		}
-		
+
 		return post.get();
 	}
-	
-	public List<Post> findByTitle(String text){
+
+	public List<Post> findByTitle(String text) {
 		return repository.searchTitle(text);
+	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repository.fullSearch(text, minDate, maxDate);
 	}
 }
